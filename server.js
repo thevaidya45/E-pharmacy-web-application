b3a3869431c8c2c47a4bdbcd0153062e5237d39a
thevaidya45 <thevaidya45@gmail.com>
2026-06-10 22:58:29 +0530

    just commit

--- server.js
+++ server.js
@@ -40,4 +40,4 @@ const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
     console.log(
         `Server Running on ${process.env.DEV_MODE} mode on port${PORT}`.bgCyan.white);
-});
+});;
